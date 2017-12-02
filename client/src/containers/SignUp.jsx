527ae1f378770120efadff1194e14806b212f12f
Brandon Hadler <brandon.hadler30@gmail.com>
2017-12-02 15:39:06 -0800

    redirect to home page upon successful sign up

--- client/src/containers/SignUp.jsx
+++ client/src/containers/SignUp.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import PropTypes from 'prop-types';
 import { signUp } from '../actions/index';
+import { Redirect } from 'react-router-dom';
 
 class SignUp extends Component {
   constructor(props) {
@@ -10,91 +11,93 @@ class SignUp extends Component {
     this.state = {
       role: 'employee',
     };
-
-    this.radioHandler = this.radioHandler.bind(this);
   }
 
-  radioHandler() {
-
-  }
-
-  render () {
+  render() {
     return (
-      <div className="credentials clear-fix">
-        <h4>Sign Up</h4>
-        <form >
-          <div>
-            <label className="credentials-label">Username:</label>
-            <input className="credentials-input" id="username" type="text" name="username" />
-          </div>
+      this.props.users ?
+        <Redirect to="/" /> :
 
-          <div>
-            <label className="credentials-label">Password:</label>
-            <input className="credentials-input" id="password" type="password" name="password" />
-          </div>
+        <div className="credentials clear-fix">
+          <h4>Sign Up</h4>
+          <form >
+            <div>
+              <label className="credentials-label">Username:</label>
+              <input className="credentials-input" id="username" type="text" name="username" />
+            </div>
 
-          <div>
-            <label className="credentials-label">Role:</label>
-            <input onChange={this.radioHandler} name="role" type="radio" value="employee" checked="checked" />Employee
-            <input onChange={this.radioHandler} name="role" type="radio" value="manager" />Manager
-          </div>
+            <div>
+              <label className="credentials-label">Password:</label>
+              <input className="credentials-input" id="password" type="password" name="password" />
+            </div>
 
-          <div>
-            <label className="credentials-label">Emergency Contact:</label>
-            <input className="credentials-input" id="emergencyContact" type="text" name="emergencyContact" />
-          </div>
+            <div>
+              <label className="credentials-label">Role:</label>
+              <input onChange={this.radioHandler} name="role" type="radio" value="employee" checked="checked" />Employee
+              <input onChange={this.radioHandler} name="role" type="radio" value="manager" />Manager
+            </div>
 
-          <div>
-            <label className="credentials-label">Address:</label>
-            <input className="credentials-input" id="address" type="text" name="address" />
-          </div>
+            <div>
+              <label className="credentials-label">Emergency Contact:</label>
+              <input className="credentials-input" id="emergencyContact" type="text" name="emergencyContact" />
+            </div>
 
-          <div>
-            <label className="credentials-label">Email:</label>
-            <input className="credentials-input" id="email" type="email" name="email" />
-          </div>
+            <div>
+              <label className="credentials-label">Address:</label>
+              <input className="credentials-input" id="address" type="text" name="address" />
+            </div>
 
-          <div>
-            <label className="credentials-label">Phone Number:</label>
-            <input className="credentials-input" id="phoneNumber" type="tel" name="phoneNumber" />
-          </div>
+            <div>
+              <label className="credentials-label">Email:</label>
+              <input className="credentials-input" id="email" type="email" name="email" />
+            </div>
 
-          <div className="btn-credentials">
-            <input className="btn-main clickable" type="button" value="Sign Up" onClick={
-              () => {
-                let username = document.getElementById('username').value;
-                let password = document.getElementById('password').value;
-                let role = this.state.role;
-                let emergencyContact = document.getElementById('emergencyContact').value;
-                let address = document.getElementById('address').value;
-                let email = document.getElementById('email').value;
-                let phoneNumber = document.getElementById('phoneNumber').value;
+            <div>
+              <label className="credentials-label">Phone Number:</label>
+              <input className="credentials-input" id="phoneNumber" type="tel" name="phoneNumber" />
+            </div>
 
-                this.props.signUp({ username, role, password, emergencyContact, email, address, phoneNumber });
+            <div className="btn-credentials">
+              <input className="btn-main clickable" type="button" value="Sign Up" onClick={
+                () => {
+                  let username = document.getElementById('username').value;
+                  let password = document.getElementById('password').value;
+                  let role = this.state.role;
+                  let emergencyContact = document.getElementById('emergencyContact').value;
+                  let address = document.getElementById('address').value;
+                  let email = document.getElementById('email').value;
+                  let phoneNumber = document.getElementById('phoneNumber').value;
 
-                document.getElementById('username').value = '';
-                document.getElementById('password').value = '';
-                this.state.role = 'employee';
-                document.getElementById('emergencyContact').value = '';
-                document.getElementById('address').value = '';
-                document.getElementById('email').value = '';
-                document.getElementById('phoneNumber').value = '';
+                  this.props.signUp({ username, role, password, emergencyContact, email, address, phoneNumber });
+
+                  document.getElementById('username').value = '';
+                  document.getElementById('password').value = '';
+                  this.state.role = 'employee';
+                  document.getElementById('emergencyContact').value = '';
+                  document.getElementById('address').value = '';
+                  document.getElementById('email').value = '';
+                  document.getElementById('phoneNumber').value = '';
+                }
               }
-            }
-             />
-          </div>
-        </form>
-      </div>
+              />
+            </div>
+          </form>
+        </div>
+      
     );
   }
-
 }
 
 function mapDispatchToProps(dispatch) {
   return bindActionCreators({ signUp }, dispatch);
 }
 
+function mapStateToProps(state) {
+  return { users: state.users };
+}
+
 SignUp.propTypes = {
   signUp: PropTypes.func.isRequired,
+  users: PropTypes.arrayOf(PropTypes.object),
 };
-export default connect(null, mapDispatchToProps)(SignUp);
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
